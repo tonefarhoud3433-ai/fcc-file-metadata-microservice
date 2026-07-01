becc1c66bcea26c44299ef6154fd1261d7efe44f
Antonios Farhood <tonefarhoud3433@gmail.com>
2026-07-01 22:16:07 +0300

    feat: complete file metadata microservice

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  try {
+    if (!req.file) {
+      return res.json({ error: "Please upload a file" });
+    }
 
-
-
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
+});
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
